878a6abb6ba04aed90961f0847d414086a26237f
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:17:39 +0800

    Initialize bugfix/safari-12-captions-not-rendering

---
+++ src/js/view/controls/rewind-display-icon.js
@@ -0,0 +1,26 @@
+import UI from 'utils/ui';
+
+export default class RewindDisplayIcon {
+    constructor(model, api, element) {
+        const iconDisplay = element.querySelector('.jw-icon');
+
+        this.el = element;
+        this.ui = new UI(iconDisplay).on('click tap enter', function() {
+            const currentPosition = model.get('position');
+            const duration = model.get('duration');
+            const rewindPosition = currentPosition - 10;
+            let startPosition = 0;
+
+            // duration is negative in DVR mode
+            if (model.get('streamType') === 'DVR') {
+                startPosition = duration;
+            }
+            // Seek 10s back. Seek value should be >= 0 in VOD mode and >= (negative) duration in DVR mode
+            api.seek(Math.max(rewindPosition, startPosition));
+        });
+    }
+
+    element() {
+        return this.el;
+    }
+}
