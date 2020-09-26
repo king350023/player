0756d60eff89e410638479a2535996d0b4b21136
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:05:36 +0800

    Initialize improvement/environment-comment

---
+++ src/js/program/media-element-pool.js
@@ -0,0 +1,87 @@
+import { MEDIA_POOL_SIZE } from 'program/program-constants';
+
+export default function MediaElementPool() {
+    const maxPrimedTags = MEDIA_POOL_SIZE;
+    const elements = [];
+    const pool = [];
+    for (let i = 0; i < maxPrimedTags; i++) {
+        const mediaElement = createMediaElement();
+        elements.push(mediaElement);
+        pool.push(mediaElement);
+    }
+
+    // Reserve an element exclusively for ads
+    const adElement = pool.shift();
+
+    // Reserve an element exclusively for feature testing.
+    const testElement = pool.shift();
+
+    return {
+        prime() {
+            elements.forEach(primeMediaElementForPlayback);
+        },
+        getPrimedElement() {
+            if (pool.length) {
+                // Shift over pop so that we cycle through elements instead of reusing the same one
+                return pool.shift();
+            }
+            return null;
+        },
+        getAdElement() {
+            return adElement;
+        },
+        getTestElement() {
+            return testElement;
+        },
+        clean(mediaElement) {
+            // Try to clean the media element so that we don't see frames of the previous video when reusing a tag
+            // We don't want to call load again if the media element is already clean
+            if (!mediaElement.src) {
+                return;
+            }
+
+            mediaElement.removeAttribute('src');
+            try {
+                mediaElement.load();
+            } catch (e) {
+                // Calling load may throw an exception, but does not result in an error state
+            }
+        },
+        recycle(mediaElement) {
+            if (mediaElement && !pool.some(element => element === mediaElement)) {
+                this.clean(mediaElement);
+                pool.push(mediaElement);
+            }
+        },
+        syncVolume: function (volume) {
+            const vol = Math.min(Math.max(0, volume / 100), 1);
+            elements.forEach(e => {
+                e.volume = vol;
+            });
+        },
+        syncMute(muted) {
+            elements.forEach(e => {
+                e.muted = muted;
+            });
+        }
+    };
+}
+
+function primeMediaElementForPlayback(mediaElement) {
+    // If we're in a user-gesture event call load() on video to allow async playback
+    if (!mediaElement.src) {
+        mediaElement.load();
+    }
+}
+
+function createMediaElement() {
+    const mediaElement = document.createElement('video');
+
+    mediaElement.className = 'jw-video jw-reset';
+    mediaElement.setAttribute('tabindex', '-1');
+    mediaElement.setAttribute('disableRemotePlayback', '');
+    mediaElement.setAttribute('webkit-playsinline', '');
+    mediaElement.setAttribute('playsinline', '');
+
+    return mediaElement;
+}
---
+++ src/js/view/controls/components/slider.js
@@ -0,0 +1,126 @@
+import sliderTemplate from 'view/controls/templates/slider';
+import { OS } from 'environment/environment';
+import Events from 'utils/backbone.events';
+import UI from 'utils/ui';
+import { between } from 'utils/math';
+import { bounds, createElement } from 'utils/dom';
+
+const getRailBounds = function(elementRail) {
+    const railBounds = bounds(elementRail);
+    // Partial workaround of Android 'inert-visual-viewport'
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=489206
+    const pageXOffset = window.pageXOffset;
+    if (pageXOffset && OS.android && document.body.parentElement.getBoundingClientRect().left >= 0) {
+        railBounds.left -= pageXOffset;
+        railBounds.right -= pageXOffset;
+    }
+    return railBounds;
+};
+
+export default class Slider {
+    constructor(className, orientation) {
+        Object.assign(this, Events);
+
+        this.className = className + ' jw-background-color jw-reset';
+        this.orientation = orientation;
+
+        this.dragStartListener = this.dragStart.bind(this);
+        this.dragMoveListener = this.dragMove.bind(this);
+        this.dragEndListener = this.dragEnd.bind(this);
+
+        this.tapListener = this.tap.bind(this);
+    }
+
+    setup() {
+        this.el = createElement(sliderTemplate(this.className, 'jw-slider-' + this.orientation));
+
+        this.elementRail = this.el.getElementsByClassName('jw-slider-container')[0];
+        this.elementBuffer = this.el.getElementsByClassName('jw-buffer')[0];
+        this.elementProgress = this.el.getElementsByClassName('jw-progress')[0];
+        this.elementThumb = this.el.getElementsByClassName('jw-knob')[0];
+
+        this.userInteract = new UI(this.element(), { preventScrolling: true });
+
+        this.userInteract.on('dragStart', this.dragStartListener);
+        this.userInteract.on('drag', this.dragMoveListener);
+        this.userInteract.on('dragEnd', this.dragEndListener);
+
+        this.userInteract.on('tap click', this.tapListener);
+    }
+
+    dragStart() {
+        this.trigger('dragStart');
+        this.railBounds = getRailBounds(this.elementRail);
+    }
+
+    dragEnd(evt) {
+        this.dragMove(evt);
+        this.trigger('dragEnd');
+    }
+
+    dragMove(evt) {
+        const railBounds = this.railBounds = (this.railBounds) ? this.railBounds : getRailBounds(this.elementRail);
+        let dimension;
+        let percentage;
+
+        if (this.orientation === 'horizontal') {
+            dimension = evt.pageX;
+            if (dimension < railBounds.left) {
+                percentage = 0;
+            } else if (dimension > railBounds.right) {
+                percentage = 100;
+            } else {
+                percentage = between((dimension - railBounds.left) / railBounds.width, 0, 1) * 100;
+            }
+        } else {
+            dimension = evt.pageY;
+            if (dimension >= railBounds.bottom) {
+                percentage = 0;
+            } else if (dimension <= railBounds.top) {
+                percentage = 100;
+            } else {
+                percentage =
+                    between((railBounds.height - (dimension - railBounds.top)) / railBounds.height, 0, 1) * 100;
+            }
+        }
+
+        this.render(percentage);
+        this.update(percentage);
+
+        return false;
+    }
+
+    tap(evt) {
+        this.railBounds = getRailBounds(this.elementRail);
+        this.dragMove(evt);
+    }
+
+    limit(percentage) {
+        // modules that extend Slider can set limits on the percentage (TimeSlider)
+        return percentage;
+    }
+
+    update(percentage) {
+        this.trigger('update', { percentage: percentage });
+    }
+
+    render(percentage) {
+        percentage = Math.max(0, Math.min(percentage, 100));
+
+        if (this.orientation === 'horizontal') {
+            this.elementThumb.style.left = percentage + '%';
+            this.elementProgress.style.width = percentage + '%';
+        } else {
+            this.elementThumb.style.bottom = percentage + '%';
+            this.elementProgress.style.height = percentage + '%';
+        }
+    }
+
+    updateBuffer(percentage) {
+        this.elementBuffer.style.width = percentage + '%';
+    }
+
+    element() {
+        return this.el;
+    }
+}
