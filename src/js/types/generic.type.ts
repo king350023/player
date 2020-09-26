ae392098ff613abd516c5673e372d40735b2c665
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:15:51 +0800

    Initialize chore/ts-10991

---
+++ src/js/types/generic.type.ts
@@ -0,0 +1,25 @@
+export type GenericObject = { [key: string]: any };
+
+export type CoreModel = any;
+
+export type PageNode = (Element | ChildNode) & {
+    baseName?: string;
+    localName?: string;
+    text?: string;
+};
+
+export type PlaylistItemType = any;
+
+export type HTMLTemplateString = string;
+
+export type PlayerAPI = any;
+
+export type PlaylistItemSource = any;
+
+export type DefaultProvider = any;
+
+export type Menu = GenericObject;
+
+export type Tooltip = GenericObject;
+
+export type Localization = GenericObject;
