0019984e9be9ed3c42690367f873d2ad3433d76d
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:14:45 +0800

    Initialize bugfix/fix-infinite-loop

---
+++ test/unit/storage-test.js
@@ -0,0 +1,29 @@
+import Storage from 'model/storage';
+import SimpleModel from 'model/simplemodel';
+import _ from 'test/underscore';
+
+describe('Storage', function() {
+
+    it('provides persistent storage', function() {
+        const model = new SimpleModel();
+        const storage = new Storage('namespace', [
+            'volume',
+            'mute',
+        ]);
+
+        storage.track(model);
+
+        model.set('volume', 70);
+        const data1 = storage.getAllItems();
+        expect(data1.volume, 'storage listens for changes to model').to.equal(70);
+
+        storage.clear();
+        const data2 = storage.getAllItems();
+        expect(_.size(data2), 'storage is empty after calling clear').to.equal(0);
+
+        model.set('mute', true);
+        const data3 = storage.getAllItems();
+        expect(_.size(data3), 'storage has one item after change to model').to.equal(1);
+        expect(data3.mute, 'boolean value stored properly').to.equal(true);
+    });
+});
