6d2a78af9bab97ca196c6c87070ed0de3f8220f4
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:09:22 +0800

    Initialize improvement/floating-after-complete

---
+++ src/js/plugins/loader.js
@@ -0,0 +1,31 @@
+import { PlayerError } from 'api/errors';
+import { configurePlugin, mapPluginToCode } from 'plugins/utils';
+
+const PluginLoader = function () {
+    this.load = function (api, pluginsModel, pluginsConfig, model) {
+        // Must be a hash map
+        if (!pluginsConfig || typeof pluginsConfig !== 'object') {
+            return Promise.resolve();
+        }
+
+        return Promise.all(Object.keys(pluginsConfig).filter(pluginUrl => pluginUrl)
+            .map(pluginUrl => {
+                const pluginConfig = pluginsConfig[pluginUrl];
+                return pluginsModel.setupPlugin(pluginUrl).then((plugin) => {
+                    if (model.attributes._destroyed) {
+                        return;
+                    }
+                    return configurePlugin(plugin, pluginConfig, api);
+                }).catch(error => {
+                    pluginsModel.removePlugin(pluginUrl);
+                    if (!error.code) {
+                        return new PlayerError(null, mapPluginToCode(pluginUrl), error);
+                    }
+                    return error;
+                });
+            }));
+    };
+
+};
+
+export default PluginLoader;
