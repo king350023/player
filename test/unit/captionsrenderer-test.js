d19717c954d0daf43967190ce7e4eba4faad91f7
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:06:50 +0800

    Initialize experiments

---
+++ test/unit/captionsrenderer-test.js
@@ -0,0 +1,31 @@
+import Model from 'controller/model';
+import ViewModel from 'view/view-model';
+import CaptionsRenderer from 'view/captionsrenderer';
+import VTTCue from 'parsers/captions/vttcue';
+
+const model = new Model({});
+const viewModel = new ViewModel(model);
+const captionsRenderer = new CaptionsRenderer(viewModel, model);
+captionsRenderer.setup('player', {});
+
+describe('CaptionsRenderer.getCurrentCues', function() {
+
+    it('should show the correct number of cues at any given position in time', function() {
+        const allCues = [
+            new VTTCue(0, 3, 'HG: Morning, Rob.'),
+            new VTTCue(4, 5, 'How are you?'),
+            new VTTCue(7, 10, 'RW: Good, and you?'),
+            new VTTCue(10, 14, 'I\'m great!'),
+            new VTTCue(12, 15, 'EG: Hey, Jo...'),
+            new VTTCue(13, 14, 'JB: Yeah?'),
+            new VTTCue(13, 14, 'JP: Yeah?'),
+            new VTTCue(16, null, 'The End')
+        ];
+        const currentNumCues = [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 2, 1, 2, 4, 4, 1, 1];
+
+        for (let i = 0; i < currentNumCues.length; i += 1) {
+            expect(captionsRenderer.getCurrentCues(allCues, i).length, 'Invalid number of cues at position ' + i).to.equal(currentNumCues[i]);
+        }
+    });
+});
+
