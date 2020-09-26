62377436f40d1379beef4f80a13a4843e5896fee
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:04:46 +0800

    Initialize feature/info-overlay

---
+++ src/js/view/controls/components/settings/menu.js
@@ -0,0 +1,169 @@
+import { cloneIcon } from 'view/controls/icons';
+import button from 'view/controls/components/button';
+import SettingsMenuTemplate from 'view/controls/templates/settings/menu';
+import { createElement, emptyElement, prependChild } from 'utils/dom';
+
+export function SettingsMenu(onVisibility, onSubmenuAdded, onMenuEmpty) {
+    const documentClickHandler = (e) => {
+        // Close if anything other than the settings menu has been clicked
+        // Let the display (jw-video) handles closing itself (display clicks do not pause if the menu is open)
+        // Don't close if the user has dismissed the nextup tooltip via it's close button (the tooltip overlays the menu)
+        if (!/jw-(settings|video|nextup-close|sharing-link)/.test(e.target.className)) {
+            instance.close();
+        }
+    };
+
+    let visible;
+    let active = null;
+    const submenus = {};
+
+    const settingsMenuElement = createElement(SettingsMenuTemplate());
+
+    const closeOnEnter = function(evt) {
+        if (evt && evt.keyCode === 27) {
+            instance.close(evt);
+            evt.stopPropagation();
+        }
+    };
+    settingsMenuElement.addEventListener('keydown', closeOnEnter);
+
+    const closeButton = button('jw-settings-close', () => {
+        instance.close();
+    }, 'Close Settings', [cloneIcon('close')]);
+
+    const closeOnButton = function(evt) {
+        // Close settings menu when enter is pressed on the close button
+        // or when tab key is pressed since it is the last element in topbar
+        if (evt.keyCode === 13 || (evt.keyCode === 9 && !evt.shiftKey)) {
+            instance.close(evt);
+        }
+    };
+    closeButton.show();
+    closeButton.element().addEventListener('keydown', closeOnButton);
+
+    const topbarElement = settingsMenuElement.querySelector('.jw-settings-topbar');
+    topbarElement.appendChild(closeButton.element());
+
+    const instance = {
+        open(isDefault, event) {
+            visible = true;
+            onVisibility(visible, event);
+            settingsMenuElement.setAttribute('aria-expanded', 'true');
+            document.addEventListener('click', documentClickHandler);
+
+            if (isDefault) {
+                if (event && event.type === 'enter') {
+                    active.categoryButtonElement.focus();
+                }
+            } else {
+                active.element().firstChild.focus();
+            }
+
+        },
+        close(event) {
+            visible = false;
+            onVisibility(visible, event);
+
+            active = null;
+            deactivateAllSubmenus(submenus);
+
+            settingsMenuElement.setAttribute('aria-expanded', 'false');
+            document.removeEventListener('click', documentClickHandler);
+        },
+        toggle() {
+            if (visible) {
+                this.close();
+            } else {
+                this.open();
+            }
+        },
+        addSubmenu(submenu) {
+            if (!submenu) {
+                return;
+            }
+            const name = submenu.name;
+            submenus[name] = submenu;
+
+            if (submenu.isDefault) {
+                prependChild(topbarElement, submenu.categoryButtonElement);
+                submenu.categoryButtonElement.addEventListener('keydown', function(evt) {
+                    // close settings menu if you shift-tab on the first category button element
+                    if (evt.keyCode === 9 && evt.shiftKey) {
+                        instance.close(evt);
+                    }
+                });
+            } else {
+                // sharing should always be the last submenu
+                const sharingButton = topbarElement.querySelector('.jw-submenu-sharing');
+                topbarElement.insertBefore(
+                    submenu.categoryButtonElement,
+                    sharingButton || closeButton.element()
+                );
+            }
+
+            settingsMenuElement.appendChild(submenu.element());
+
+            onSubmenuAdded();
+        },
+        getSubmenu(name) {
+            return submenus[name];
+        },
+        removeSubmenu(name) {
+            const submenu = submenus[name];
+            if (!submenu || submenu.element().parentNode !== settingsMenuElement) {
+                return;
+            }
+            settingsMenuElement.removeChild(submenu.element());
+            topbarElement.removeChild(submenu.categoryButtonElement);
+            submenu.destroy();
+            delete submenus[name];
+
+            if (!Object.keys(submenus).length) {
+                this.close();
+                onMenuEmpty();
+            }
+        },
+        activateSubmenu(name) {
+            const submenu = submenus[name];
+            if (!submenu || submenu.active) {
+                return;
+            }
+
+            deactivateAllSubmenus(submenus);
+            submenu.activate();
+            active = submenu;
+
+            if (!submenu.isDefault) {
+                active.element().firstChild.focus();
+            }
+        },
+        activateFirstSubmenu() {
+            const firstSubmenuName = Object.keys(submenus)[0];
+            this.activateSubmenu(firstSubmenuName);
+        },
+        element() {
+            return settingsMenuElement;
+        },
+        destroy() {
+            this.close();
+            settingsMenuElement.removeEventListener('keydown', closeOnEnter);
+            closeButton.element().removeEventListener('keydown', closeOnButton);
+            emptyElement(settingsMenuElement);
+        }
+    };
+
+    Object.defineProperties(instance, {
+        visible: {
+            enumerable: true,
+            get: () => visible
+        },
+    });
+
+    return instance;
+}
+
+const deactivateAllSubmenus = (submenus) => {
+    Object.keys(submenus).forEach(name => {
+        submenus[name].deactivate();
+    });
+};
---
+++ test/unit/playlist-test.js
@@ -0,0 +1,49 @@
+import Playlist from 'playlist/playlist';
+import Item from 'playlist/item';
+import Source from 'playlist/source';
+import _ from 'test/underscore';
+import mp4 from 'data/mp4';
+import track from 'playlist/track';
+
+function isValidPlaylistItem(playlistItem) {
+    return _.isObject(playlistItem) && _.isArray(playlistItem.sources) && _.isArray(playlistItem.tracks);
+}
+
+describe('playlist', function() {
+
+    it('Test initialized successfully', function() {
+
+        expect(typeof Item, 'item is defined').to.equal('function');
+        expect(typeof Source, 'source is defined').to.equal('function');
+        expect(typeof track, 'track is defined').to.equal('function');
+    });
+
+    it('Test constructor with single item', function() {
+        var p;
+
+        p = Playlist(mp4.starscape);
+        expect(isValidPlaylistItem(p[0]), 'Initialize single item').to.be.true;
+
+        p = Playlist(undefined);
+        expect(isValidPlaylistItem(p[0]), 'Initialize with undefined item').to.be.true;
+
+        // TODO: this doesn't actually work, shouldn't pass
+        p = Playlist(mp4.starscape.file);
+        expect(isValidPlaylistItem(p[0]), 'Initialize with just file name').to.be.true;
+    });
+
+    it('Test constructor with array of items', function() {
+        var p;
+        var arr = [mp4.starscape, mp4.starscape, mp4.starscape];
+
+        p = Playlist(arr);
+        expect(p.length, 'Same number of items initialized').to.equal(arr.length);
+
+        p = Playlist([mp4.starscape]);
+        expect(isValidPlaylistItem(p[0]), 'Initialize single item array').to.be.true;
+
+        // TODO: inconsistent, this is the only case where it returns an empty array
+        p = Playlist([]);
+        expect(_.isArray(p) && p.length === 0, 'Initialize with an empty array as argument').to.be.true;
+    });
+});
