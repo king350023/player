d5b7f5e3bc13f3efe0e09da4e72ccebbaae800cf
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:08:01 +0800

    Initialize bugfix/provider-error-code

---
+++ src/js/view/controls/components/button.js
@@ -0,0 +1,54 @@
+import UI from 'utils/ui';
+import svgParse from 'utils/svgParser';
+import { addFocusHandler } from 'utils/dom';
+
+export default function (icon, apiAction, ariaText, svgIcons) {
+    const element = document.createElement('div');
+    element.className = 'jw-icon jw-icon-inline jw-button-color jw-reset ' + icon;
+    element.setAttribute('role', 'button');
+    element.setAttribute('tabindex', '0');
+
+    if (ariaText) {
+        element.setAttribute('aria-label', ariaText);
+    }
+
+    element.style.display = 'none';
+
+    if (apiAction) {
+        new UI(element).on('click tap enter', function(event) {
+            apiAction(event);
+        });
+    }
+
+    // prevent button from having a border when focused through click
+    addFocusHandler(element);
+
+    if (svgIcons) {
+        Array.prototype.forEach.call(svgIcons, svgIcon => {
+            if (typeof svgIcon === 'string') {
+                element.appendChild(svgParse(svgIcon));
+            } else {
+                element.appendChild(svgIcon);
+            }
+        });
+    }
+
+    return {
+        element: function() {
+            return element;
+        },
+        toggle: function(m) {
+            if (m) {
+                this.show();
+            } else {
+                this.hide();
+            }
+        },
+        show: function() {
+            element.style.display = '';
+        },
+        hide: function() {
+            element.style.display = 'none';
+        }
+    };
+}
