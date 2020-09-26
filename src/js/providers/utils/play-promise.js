9a6ff41da881ae2715b92cfaf7fa224bf616eaba
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:06:06 +0800

    Initialize bugfix/standardized-error-message-cleanup

---
+++ src/js/providers/utils/play-promise.js
@@ -0,0 +1,31 @@
+import Promise from 'polyfills/promise';
+
+export default function createPlayPromise(video) {
+    return new Promise(function(resolve, reject) {
+        if (video.paused) {
+            return reject(new Error('Play refused.'));
+        }
+        const removeEventListeners = function() {
+            video.removeEventListener('play', playListener);
+            video.removeEventListener('playing', listener);
+            video.removeEventListener('pause', listener);
+            video.removeEventListener('abort', listener);
+            video.removeEventListener('error', listener);
+        };
+        const playListener = function() {
+            video.addEventListener('playing', listener);
+            video.addEventListener('abort', listener);
+            video.addEventListener('error', listener);
+            video.addEventListener('pause', listener);
+        };
+        const listener = function(e) {
+            removeEventListeners();
+            if (e.type === 'playing') {
+                resolve();
+            } else {
+                reject(new Error('The play() request was interrupted by a "' + e.type + '" event.'));
+            }
+        };
+        video.addEventListener('play', playListener);
+    });
+}
