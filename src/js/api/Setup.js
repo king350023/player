36f751a5d63b6021d5df8f56c203216f4231a824
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:05:51 +0800

    Initialize bugfix/single-quotes-import

---
+++ src/js/api/Setup.js
@@ -0,0 +1,43 @@
+import loadCoreBundle from 'api/core-loader';
+import startSetup from 'api/setup-steps';
+import loadPlugins from 'plugins/plugins';
+import { PlayerError, SETUP_ERROR_TIMEOUT, MSG_CANT_LOAD_PLAYER } from 'api/errors';
+import Promise from 'polyfills/promise';
+
+const SETUP_TIMEOUT_SECONDS = 30;
+
+const Setup = function(_model) {
+
+    let _setupFailureTimeout;
+
+    this.start = function (api) {
+
+        const setup = startSetup(_model, api, [
+            loadCoreBundle(_model),
+            loadPlugins(_model, api)
+        ]);
+
+        const timeout = new Promise((resolve, reject) => {
+            _setupFailureTimeout = setTimeout(() => {
+                const error = new PlayerError(MSG_CANT_LOAD_PLAYER, SETUP_ERROR_TIMEOUT);
+                reject(error);
+            }, SETUP_TIMEOUT_SECONDS * 1000);
+            const timeoutCancelled = () => {
+                clearTimeout(_setupFailureTimeout);
+                resolve();
+            };
+            setup.then(timeoutCancelled).catch(timeoutCancelled);
+        });
+
+        return Promise.race([setup, timeout]);
+    };
+
+    this.destroy = function() {
+        clearTimeout(_setupFailureTimeout);
+        _model.set('_destroyed', true);
+        _model = null;
+    };
+
+};
+
+export default Setup;
---
+++ test/unit/playlist-test.js
@@ -0,0 +1,61 @@
+import Playlist, { validatePlaylist } from 'playlist/playlist';
+import Item from 'playlist/item';
+import Source from 'playlist/source';
+import _ from 'test/underscore';
+import mp4 from 'data/mp4';
+import track from 'playlist/track';
+import { MSG_CANT_PLAY_VIDEO } from 'api/errors';
+
+function isValidPlaylistItem(playlistItem) {
+    return _.isObject(playlistItem) && _.isArray(playlistItem.sources) && _.isArray(playlistItem.tracks);
+}
+
+describe('playlist', function() {
+    it('initializes a valid playlist', function() {
+        expect(typeof Item, 'item is defined').to.equal('function');
+        expect(typeof Source, 'source is defined').to.equal('function');
+        expect(typeof track, 'track is defined').to.equal('function');
+    });
+
+    it('constructs a playlist from a single item', function() {
+        let p;
+
+        p = Playlist(mp4.starscape);
+        expect(isValidPlaylistItem(p[0]), 'Initialize single item').to.be.true;
+
+        p = Playlist(undefined);
+        expect(isValidPlaylistItem(p[0]), 'Initialize with undefined item').to.be.true;
+
+        // TODO: this doesn't actually work, shouldn't pass
+        p = Playlist(mp4.starscape.file);
+        expect(isValidPlaylistItem(p[0]), 'Initialize with just file name').to.be.true;
+    });
+
+    it('constructs a playlist from an arry of items', function() {
+        let p;
+        const arr = [mp4.starscape, mp4.starscape, mp4.starscape];
+
+        p = Playlist(arr);
+        expect(p.length, 'Same number of items initialized').to.equal(arr.length);
+
+        p = Playlist([mp4.starscape]);
+        expect(isValidPlaylistItem(p[0]), 'Initialize single item array').to.be.true;
+
+        // TODO: inconsistent, this is the only case where it returns an empty array
+        p = Playlist([]);
+        expect(_.isArray(p) && p.length === 0, 'Initialize with an empty array as argument').to.be.true;
+    });
+
+    it('throws a PlayerError if given a non-array object or empty array', function () {
+        [{}, [], null, undefined, ''].forEach(playlist => {
+            try {
+                validatePlaylist(playlist);
+                expect.fail('Should have thrown an error');
+            } catch (e) {
+                expect(e.key).to.equal(MSG_CANT_PLAY_VIDEO);
+                expect(e.code).to.equal(630);
+                expect(e.sourceError).to.not.exist;
+            }
+        });
+    });
+});
