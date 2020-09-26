97cef361ef8443b9bfd18e26516b3ee574b3178f
king350023 <46060641+king350023@users.noreply.github.com>
2020-09-27 01:10:03 +0800

    Initialize feature/disable_nextup_config_opt

---
+++ src/js/view/utils/clickhandler.js
@@ -0,0 +1,55 @@
+import { CLICK } from 'events/events';
+import UI from 'utils/ui';
+import Events from 'utils/backbone.events';
+
+export default class ClickHandler {
+    constructor(model, element) {
+        Object.assign(this, Events);
+
+        this.revertAlternateClickHandlers();
+        this.domElement = element;
+        this.model = model;
+
+        this.ui = new UI(element).on('click tap', this.clickHandler, this)
+            .on('doubleClick doubleTap', function() {
+                if (this.alternateDoubleClickHandler) {
+                    this.alternateDoubleClickHandler();
+                    return;
+                }
+                this.trigger('doubleClick');
+            }, this);
+    }
+
+    destroy() {
+        if (this.ui) {
+            this.ui.destroy();
+            this.ui = this.domElement = this.model = null;
+            this.revertAlternateClickHandlers();
+        }
+    }
+
+    clickHandler(evt) {
+        if (this.model.get('flashBlocked')) {
+            return;
+        }
+        if (this.alternateClickHandler) {
+            this.alternateClickHandler(evt);
+            return;
+        }
+        this.trigger((evt.type === CLICK) ? 'click' : 'tap');
+    }
+
+    element() {
+        return this.domElement;
+    }
+
+    setAlternateClickHandlers(clickHandler, doubleClickHandler) {
+        this.alternateClickHandler = clickHandler;
+        this.alternateDoubleClickHandler = doubleClickHandler || null;
+    }
+
+    revertAlternateClickHandlers() {
+        this.alternateClickHandler = null;
+        this.alternateDoubleClickHandler = null;
+    }
+}
---
+++ test/unit/playlist-loader-test.js
@@ -0,0 +1,65 @@
+import PlaylistLoader from 'playlist/loader';
+import { PLAYLIST_LOADED, ERROR } from 'events/events';
+import { MSG_CANT_PLAY_VIDEO } from 'api/errors';
+
+describe('playlist/loader', function() {
+    this.timeout(5000);
+
+    it('loads a valid JSON feed', function () {
+        const expectedJSON = [{
+            file: 'http://content.bitsontherun.com/videos/3XnJSIm4-52qL9xLP.mp4'
+        }, {
+            file: 'http://content.bitsontherun.com/videos/nfSyO85Q-27m5HpIu.webm'
+        }];
+
+        return expectPlaylistLoad('/base/test/files/playlist.json')
+            .then(data => {
+                expect(data.playlist[0].file, 'JSON successfully loaded as a playlist').to.equal(expectedJSON[0].file);
+                expect(data.playlist[1].file, 'JSON successfully loaded as a playlist').to.equal(expectedJSON[1].file);
+            });
+    });
+
+    it('loads a valid XML feed', function () {
+        const mediaid = 'TQjoCPTk';
+        return expectPlaylistLoad('/base/test/files/playlist.xml')
+            .then(function (data) {
+                expect(data.playlist.length > 0, 'Playlist has at least 1 item.').to.be.true;
+                expect(data.playlist[0].mediaid, 'Playlist item contains a mediaid.').to.equal(mediaid);
+                expect(data.playlist[0].sources.length > 0, 'Playlist item has at least one video source.').to.be.true;
+            });
+    });
+
+    it('throws a PlayerError when loading an invalid JSON feed', function () {
+        return expectLoaderError('/base/test/files/invalid.json')
+            .then(e => {
+                expect(e.code).to.equal(621);
+                expect(e.key).to.equal(MSG_CANT_PLAY_VIDEO);
+            });
+    });
+
+    it('throws a PlayerError when loading an invalid XML feed', function () {
+        return expectLoaderError('/base/test/files/invalid.xml')
+            .then(e => {
+                expect(e.code).to.equal(621);
+                expect(e.key).to.equal(MSG_CANT_PLAY_VIDEO);
+            });
+    });
+});
+
+function expectPlaylistLoad(url) {
+    return new Promise((resolve, reject) => {
+        const loader = new PlaylistLoader();
+        loader.on(PLAYLIST_LOADED, resolve);
+        loader.on(ERROR, error => reject(new Error(`Expected loader to load "${url}". Got error: "${error.message}".`)));
+        loader.load(url);
+    })
+}
+
+function expectLoaderError(url) {
+    return new Promise((resolve, reject) => {
+        const loader = new PlaylistLoader();
+        loader.on(PLAYLIST_LOADED, data => reject(new Error(`Expected loader to trigger an error for "${url}".`)));
+        loader.on(ERROR, resolve);
+        loader.load(url);
+    })
+}
